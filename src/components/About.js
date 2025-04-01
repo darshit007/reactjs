6e5c04d848e18f18a7807004d8d1a8d2d36cfb0f
darshit007 <dddmmm007@gmail.com>
2025-04-01 00:12:21 -0400

    Add class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,26 @@
-const About = () => {
+import UserClass from "./UserClass";
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About Class Component</h1>
+        <h2>This is a food delivery webapp </h2>
+        <UserClass name={"Darshit Modi - class"} location={"Toronto class"} />
+        <UserClass name={"Deadman - class"} location={"Ahmedabad class"} />
+      </div>
+    );
+  }
+}
+
+const About1 = () => {
   return (
     <div>
       <h1>About</h1>
       <h2>This is a food delivery webapp </h2>
+      <UserClass name={"Darshit Modi - class"} location={"Toronto class"} />
+      <UserClass name={"Deadman - class"} location={"Ahmedabad class"} />
     </div>
   );
 };
